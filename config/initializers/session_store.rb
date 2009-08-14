17cc59aff3cf197986e18aa009c95237398cdbdc
Nobuaki Ito <nito@gilt.jp>
2009-08-14 16:12:45 +0900

    http://f.gilt.jp/projects/3-tech/issues/5203
    Let's get the exercise listed in chapter 4.4 of the agile rails book.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_depot_session',
+  :secret      => '5314979a61fd7a281b0e97011d39adcd2fe43b7782d337e627dd528855aa9bd20285346c891373a25f05368f5e3eb13b5896906452b788bfb8188ab93f5ea634'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
