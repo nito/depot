0c6c0669c1cdff46484db80e198ed04360a0a6dc
Nobuaki Ito <nito@gilt.jp>
2009-08-15 16:25:52 +0900

    http://f.gilt.jp/projects/3-tech/issues/5209
    Added some files... around page 91 in the PDF copy of the rails book.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,:message => 'must be a URL for GIF, JPG ' + 'or PNG image.'
 end
