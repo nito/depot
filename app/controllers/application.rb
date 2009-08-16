a9f561ff79fc5e03c83a5ee23fe56d799f45c672
Nobuaki Ito <nito@gilt.jp>
2009-08-16 22:56:43 +0900

    http://f.gilt.jp/projects/3-tech/issues/5262
    The coding is done for chapter 8. Now all I need to do is add exercise
    related code.

---
+++ app/controllers/application.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+#  protect_from_forgery :secret => '8fc080370e56e929a2d5afca5540a0f7'
+
+  # See ActionController::Base for details
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password").
+  # filter_parameter_logging :password
+end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,4 +4,35 @@ class StoreController < ApplicationController
     @time = Time.now.strftime "%Y/%m/%d %H:%M"
   end
 
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    flash[:notice] = "Invalid product"
+    redirect_to :action => 'index'
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
+#    flash[:notice] = "Your cart is currently empty"
+#    redirect_to :action => 'index'
+  end
+
+private
+def redirect_to_index(msg)
+  flash[:notice] = msg
+  redirect_to :action => 'index'
+end
+
+
+private
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
+
 end
