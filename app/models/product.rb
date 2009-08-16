fbaa6673aa33cb437858b8548e20a567573c17d3
Nobuaki Ito <nito@gilt.jp>
2009-08-16 19:12:02 +0900

    http://f.gilt.jp/projects/3-tech/issues/5209
    Modified the messages shown for validation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
-  validates_presence_of :title, :description, :image_url
+  validates_presence_of :title, :description, :image_url, :message => '空であってはなりません'
   validates_length_of :title, :minimum => 10, :message => "最低10文字入力して下さい"
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
-  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,:message => 'must be a URL for GIF, JPG ' + 'or PNG image.'
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,:message => 'PNG/GIF/JPGのいずれかでなければなりません'
   protected
   def price_must_be_at_least_a_cent
-    errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+    errors.add(:price, '最低1セントでなければなりません') if price.nil? || price < 0.01
   end
 
 end
