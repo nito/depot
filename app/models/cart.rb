d54be83c7bad44855a1564cd6084c4cd3947eb00
Nobuaki Ito <nito@gilt.jp>
2009-08-22 16:46:25 +0900

    http://wiki.github.com/nito/depot/memo-on-chapter-9
    Now cart flashes when an item is added to the shopping cart. Very
    interesting.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,17 +5,19 @@ class Cart
     @items = []
   end
 
-  def total_price 
-    @items.sum { |item| item.price } 
-  end 
+  def total_price
+    @items.sum { |item| item.price }
+  end
 
   def add_product(product)
     current_item = @items.find {|item| item.product == product}
     if current_item
       current_item.increment_quantity
     else
-      @items << CartItem.new(product)
+      current_item = CartItem.new(product)
+      @items << current_item
     end
+    current_item
   end
 
 end
